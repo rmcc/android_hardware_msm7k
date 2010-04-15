6ba773262add42ad7c615c090260f2d53485820f
rmcc <github@cerqueira.org>
2010-04-15 11:38:50 +0100

    Import librpc changes from CodeAurora

--- librpc/clnt.c
+++ librpc/clnt.c
@@ -1,3 +1,31 @@
+/* Copyright (c) 2009, Code Aurora Forum. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Code Aurora nor
+ *       the names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior written
+ *       permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 #include <rpc/rpc.h>
 #include <arpa/inet.h>
 #include <rpc/rpc_router_ioctl.h>
@@ -9,6 +37,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <poll.h>
 
 struct CLIENT {
     xdr_s_type *xdr;
@@ -116,7 +145,23 @@ static void *cb_context(void *__u)
                       (uint32_t)prog, (int)vers);
                     xdr_destroy_common(*svc_xdr);
                 }
-                
+
+                /* Do these checks before the clone */
+                if (client->xdr->in_len < 0) {
+                    E("%08x:%08x xdr->in_len = %i error %s (%d)",
+                        client->xdr->in_len,
+                        client->xdr->x_prog, client->xdr->x_vers,
+                        strerror(errno), errno);
+                    continue;
+                }
+                if (client->xdr->out_next < 0) {
+                    E("%08x:%08x xdr->out_next = %i error %s (%d)",
+                        client->xdr->out_next,
+                        client->xdr->x_prog, client->xdr->x_vers,
+                    strerror(errno), errno);
+                    continue;
+                }
+
                 D("%08x:%08x cloning XDR for "
                   "callback client %08x:%08x.\n",
                   client->xdr->x_prog,
@@ -169,11 +214,27 @@ static void *rx_context(void *__u __attribute__((unused)))
     int n;
     struct timeval tv;
     fd_set rfds;
+    int ret;
+    struct pollfd fd;
+    int timeout = 0;
+
+    memset((void *) &fd, 0, sizeof(fd));
     while(num_clients) {
         pthread_mutex_lock(&rx_mutex);
         rfds = rx_fdset;
         pthread_mutex_unlock(&rx_mutex);
         tv.tv_sec = 0; tv.tv_usec = 500 * 1000;
+
+        ret = poll(&fd, 1, timeout);
+        if (ret > 0) {
+            if (fd.revents & POLLERR) {
+                D("Modem is resetting.\n");
+                timeout = 500;
+                continue;
+            }
+        }
+        timeout = 0;
+
         n = select(max_rxfd + 1, (fd_set *)&rfds, NULL, NULL, &tv);
         if (n < 0) {
             E("select() error %s (%d)\n", strerror(errno), errno);
@@ -204,7 +265,24 @@ static void *rx_context(void *__u __attribute__((unused)))
                     }
                     D("%08x:%08x reading data.\n",
                       client->xdr->x_prog, client->xdr->x_vers);
-                    client->xdr->xops->read(client->xdr);
+                    ret = client->xdr->xops->read(client->xdr);
+                    if (ret == FALSE) {
+                        E("%08x:%08x xops->read() error %s (%d)\n",
+                          client->xdr->x_prog, client->xdr->x_vers,
+                        strerror(errno), errno);
+
+                        if (errno == ENETRESET) {
+                            E("%08x:%08x clearing reset.\n",
+                                client->xdr->x_prog, client->xdr->x_vers);
+                                client->xdr->xops->xdr_control(
+                                client->xdr,
+                                RPC_ROUTER_IOCTL_CLEAR_NETRESET, NULL);
+                           fd.fd = client->xdr->fd;
+                        }
+
+                        pthread_mutex_unlock(&client->input_xdr_lock);
+                        continue;
+                    }
                     client->input_xdr_busy = 1;
                     pthread_mutex_unlock(&client->input_xdr_lock);
 
@@ -521,6 +599,11 @@ CLIENT *clnt_create(
         char name[256];
 
         pthread_mutex_lock(&rx_mutex);
+
+ 
+        /* Implment backwards compatibility */
+        vers = (vers & 0x80000000) ? vers : vers & 0xFFFF0000;
+
 
         snprintf(name, sizeof(name), "/dev/oncrpc/%08x:%08x",
                  (uint32_t)prog, (int)vers);
